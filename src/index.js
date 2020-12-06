d3b5e66153198cbcb8de4a334063a848e7c90d48
Kimmi Gan <kimmi.gan@kopijunkie.net>
2020-12-06 20:23:51 +0000

    Post tweet if channel is live

---
+++ src/index.js
@@ -0,0 +1,46 @@
+const Twit = require("twit");
+const TwitchJs = require("twitch-js").default;
+const { twitchConfig, twitterConfig } = require("./config.js");
+
+const connectToTwitch = () => {
+  // To get a temp token for dev: https://twitchtokengenerator.com/
+  // TODO: Get Twitch OAuth Token
+  const tempToken = "TWITCH_OAUTH_TOKEN";
+  const twitchUsername = twitchConfig.username;
+  const twitchJs = new TwitchJs({
+    clientId: twitchConfig.client_id,
+    token: tempToken,
+    onAuthenticationFailure: () => {
+      console.error("* Twitch auth error!");
+    },
+  });
+  twitchJs.api.get(`search/channels?query=${twitchUsername}`).then(
+    (response) => {
+      console.log(response);
+      const streamData = response.data[0];
+      if (streamData.isLive) {
+        postTweet(streamData.title);
+      } else {
+        console.log(`${twitchUsername} is offline right now...`);
+      }
+    },
+    (error) => {
+      console.error("* Twitch auth error!", error);
+    }
+  );
+};
+
+const postTweet = (streamTitle = "") => {
+  const Twitter = new Twit(twitterConfig);
+  Twitter.post(
+    "statuses/update",
+    {
+      status: `${streamTitle}...come hang out ${twitchConfig.channel_url}`,
+    },
+    (err, data, response) => {
+      console.log(data);
+    }
+  );
+};
+
+connectToTwitch();
